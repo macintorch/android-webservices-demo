c4a9e74fafb1833675ce0ec022e0c76c1fc64a21
Ainor Syahrizal <macintorch@gmail.com>
2017-01-10 15:16:27 +0800

    settings connect the webpage

--- app/src/main/java/ainor/com/my/webservicesdemo/MainActivity.java
+++ app/src/main/java/ainor/com/my/webservicesdemo/MainActivity.java
@@ -2,6 +2,8 @@ package ainor.com.my.webservicesdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("http://www.ainor.com.my");
+
+
     }
 }
